d1fc7dba5b712fd9c6c32288cbc6accad6e6a865
miachafer <miachafer@gmail.com>
2022-07-31 23:01:41 -0300

    Adiciona funções de validação de nome e endereço

--- ecommerce/cliente/serializers.py
+++ ecommerce/cliente/serializers.py
@@ -5,3 +5,21 @@ class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = "__all__"
+
+    """
+    Validações
+    As funções a seguir realizam duas validações:
+    1. valida se o nome fornecido tem duas palvras (nome e sobrenome)
+    2. valida se o endereço fornecido é composto por pelo menos uma letra
+    """
+    def validate_nome(self, nome):
+        nome_completo = nome.split(" ")
+        if len(nome_completo) == 1:
+            raise serializers.ValidationError("Forneça pelo menos um nome e um sobrenome.")
+        return nome
+    
+    def validate_endereco(self, endereco):
+        existe_letra = any(letra.isalpha() for letra in endereco)
+        if existe_letra == False:
+            raise serializers.ValidationError("Forneça um endereço com letras.")
+        return endereco
