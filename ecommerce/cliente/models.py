fbc019b87af7ee6485bb929e9e30c374dd7e0245
miachafer <miachafer@gmail.com>
2022-08-02 13:35:43 -0300

    Atualiza campo CPF e campo telefone

--- ecommerce/cliente/models.py
+++ ecommerce/cliente/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from localflavor.br.models import BRCPFField
-from django.core.validators import RegexValidator
 
 """
 O modelo Cliente é composto por dados pessoais e de contato:
@@ -12,32 +11,28 @@ Nenhum dos campos pode ser deixado em branco.
 O telefone é validado com uso de uma expressão regular que só permite a criação de uma instância do modelo cliente se o número tiver um formato compatível com o padrão brasileiro. 
 """
 
-REGEX_TELEFONE = RegexValidator(r'^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$', 'Insira um número de telefone válido. Telefones móveis: (XX) 9XXXX-XXXX Telefones fixos: (XX) XXXX-XXXX')
-
 class Cliente(models.Model):
     nome = models.CharField(
         max_length=255,
         verbose_name="Nome completo",
         blank=False)
-    cpf = models.BRCPFField(
-        "CPF",
+    cpf = BRCPFField(
         max_length=11,
         primary_key=True,
         verbose_name="CPF",
         help_text="Digite seu CPF no formato 11122233344")
     endereco = models.TextField(verbose_name="Endereço", blank=False)
     telefone = models.CharField(
-        max_length=15,
-        help_text="Digite o telefone nos formatos: (XX) XXXX-XXXX ou (XX) 9XXXX-XXXX",
-        validators=[REGEX_TELEFONE],
+        max_length=11,
+        help_text="Digite o telefone nos formatos: DDD (2 dígitos) + número (8 ou 9 dígitos) sem espaços",
         unique=True,
         blank=False
     )
     email = models.EmailField(max_length=255, unique=True, blank=False)
-    criado_em = models.DateTimeField(auto_add_now=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'Nome: {self.nome} - CPF: {self.cpf}'
-    
+
     
