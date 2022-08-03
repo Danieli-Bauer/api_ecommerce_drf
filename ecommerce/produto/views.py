834d8b1fb1eee2f75d596f636589925b10d40440
nairasn <nascimentosnaira@gmail.com>
2022-08-03 19:57:37 -0300

    Adiciona viewset de Produto

--- ecommerce/produto/views.py
+++ ecommerce/produto/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Produto
+from .serializers import ProdutoSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class ProdutoViewSet(viewsets.ModelViewSet):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
