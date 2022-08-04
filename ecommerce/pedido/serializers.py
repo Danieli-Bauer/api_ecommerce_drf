491824dca79d6a0c8b1d9e7a4ca8c12d88bcdbf1
Danieli-Bauer <danielibauer60@gmail.com>
2022-08-04 20:10:39 -0300

    Adiciona validação da descrição de pedido

--- ecommerce/pedido/serializers.py
+++ ecommerce/pedido/serializers.py
@@ -7,6 +7,16 @@ class PedidoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pedido
         fields = '__all__'
+    
+    """
+    A função a seguir evita que o usuário insira somente números como descrição.
+    """    
+    def validate_descricao(self, descricao):
+        so_numero = descricao.isnumeric()
+        if so_numero == True:
+            raise serializers.ValidationError("Descrição do produto não pode conter só números. Forneça uma nova descrição.")
+        return descricao
+
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
