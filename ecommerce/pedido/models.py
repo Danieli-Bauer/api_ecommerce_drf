763ad102e3ade4fc631c1f4239022b4ade7a3491
Danieli-Bauer <danielibauer60@gmail.com>
2022-08-04 19:49:56 -0300

    Adiciona model pedido

--- ecommerce/pedido/models.py
+++ ecommerce/pedido/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from ecommerce.cliente.models import Cliente
+import uuid
 
-# Create your models here.
+class Pedido(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    descricao = models.TextField(blank=False)
+    client = models.ForeignKey(Cliente, on_delete=models.CASCADE, blank=False)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"Pedido: {self.id} - Descrição: {self.descricao}"
+
