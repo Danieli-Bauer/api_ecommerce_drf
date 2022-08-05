4d241c58bd536eae5c09dea8da2e57ae5a4d68b7
miachafer <miachafer@gmail.com>
2022-08-04 21:09:29 -0300

    Adiciona viewsets simples para Pedido e Item

--- ecommerce/pedido/views.py
+++ ecommerce/pedido/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import PedidoSerializer, ItemSerializer
+from .models import Pedido, Item
 
-# Create your views here.
+class PedidoViewSet(viewsets.ModelViewSet):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
