00988f10ca334362b18b151752b17693bc788b17
miachafer <miachafer@gmail.com>
2022-08-04 21:08:19 -0300

    Atualiza model Item

--- ecommerce/pedido/models.py
+++ ecommerce/pedido/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ecommerce.cliente.models import Cliente
+from ecommerce.produto.models import Produto
 import uuid
 from django.core.validators import MinValueValidator
 
@@ -16,9 +17,12 @@ class Pedido(models.Model):
 class Item(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE, blank=False)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE, blank=False)
     quantidade = models.IntegerField(
         blank=False,
         validators=[MinValueValidator(0)])
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return f"{self.id} - Pedido: {self.pedido} - Produto: {self.produto} - Quantidade: {self.quantidade}"
