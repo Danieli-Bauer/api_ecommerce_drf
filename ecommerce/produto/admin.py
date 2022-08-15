4d281f50da7513843c06ff5a62b9b94dfc247e29
nairasn <nascimentosnaira@gmail.com>
2022-08-15 18:36:40 -0300

    adiciona config admin produto

--- ecommerce/produto/admin.py
+++ ecommerce/produto/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from .models import Produto
 
-# Register your models here.
+# admin.site.register(Produto)
 
-admin.site.register(Produto)
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'descricao','preco', 'quantidade', 'get_criado_em', 'get_atualizado_em',)
+    list_filter = ('nome', 'preco',)
+    search_fields = ('nome', 'descricao', 'preco',)
+    list_display_links = ('nome',)
+    
+    def get_criado_em(self, obj):
+        if obj.criado_em:
+            return obj.criado_em.strftime('%d/%m/%Y')
+    
+    get_criado_em.short_description = 'Criado em'
+    
+    def get_atualizado_em(self, obj):
+        if obj.criado_em:
+            return obj.criado_em.strftime('%d/%m/%Y')
+    
+    get_atualizado_em.short_description = 'Atualizado em'
+    
+    
