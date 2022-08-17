5f9efb46286c7012e634648bfa0c52e17ab5e72f
miachafer <miachafer@gmail.com>
2022-08-17 14:24:11 -0300

    Resolve alguns bugs do admin

--- ecommerce/pedido/admin.py
+++ ecommerce/pedido/admin.py
@@ -7,7 +7,7 @@ from .models import Pedido, Item
 class PedidoAdmin(admin.ModelAdmin):
     list_display = ('id', 'cliente', 'descricao', 'get_criado_em', 'get_atualizado_em',)
     list_filter = ('cliente',)
-    search_fields = ('nome', 'descricao',)
+    search_fields = ['id', 'cliente__nome', 'cliente__cpf', 'descricao',]
     list_display_links = ('cliente',)
     
     def get_criado_em(self, obj):
@@ -17,8 +17,8 @@ class PedidoAdmin(admin.ModelAdmin):
     get_criado_em.short_description = 'Criado em'
     
     def get_atualizado_em(self, obj):
-        if obj.criado_em:
-            return obj.criado_em.strftime('%d/%m/%Y')
+        if obj.atualizado_em:
+            return obj.atualizado_em.strftime('%d/%m/%Y')
     
     get_atualizado_em.short_description = 'Atualizado em'
     
@@ -27,7 +27,7 @@ class PedidoAdmin(admin.ModelAdmin):
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('id', 'pedido', 'produto', 'quantidade', 'get_criado_em', 'get_atualizado_em',)
     list_filter = ('pedido', 'produto',)
-    search_fields = ('pedido', 'produto',)
+    search_fields = ['pedido__id', 'pedido__descricao', 'produto__nome',]
     list_display_links = ('pedido', 'produto',)
     
     def get_criado_em(self, obj):
@@ -37,8 +37,8 @@ class ItemAdmin(admin.ModelAdmin):
     get_criado_em.short_description = 'Criado em'
     
     def get_atualizado_em(self, obj):
-        if obj.criado_em:
-            return obj.criado_em.strftime('%d/%m/%Y')
+        if obj.atualizado_em:
+            return obj.atualizado_em.strftime('%d/%m/%Y')
     
     get_atualizado_em.short_description = 'Atualizado em'
     
